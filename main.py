c1f82b3f4c409846279f821e8fe3488e21834971
Rasim Aghayev <rasimaqayev@gmail.com>
2022-04-07 21:46:48 +0000

    Forms

--- main.py
+++ main.py
@@ -1,51 +1,7 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional,List
+from fastapi import FastAPI,Form
 
-class Todo(BaseModel):
-    name: str
-    due_date:str
-    description:str
+app=FastAPI()
 
-app=FastAPI(title="ToDo API")
-
-#Create, Read, update, Delete (CRUD)
-
-store_todo=[]
-
-@app.get("/")
-async def home():
-    return{"hello":"World"}
-
-@app.post("/todo/")
-async def create_todo(todo: Todo):
-    store_todo.append(todo)
-    return todo
-
-@app.get("/todo/", response_model=List[Todo])
-async def get_all_todos():
-    return store_todo
-
-@app.get('/todo/{id}')
-async def get_todo(id: int):
-    try:
-        return store_todo[id]
-    except:
-        raise HTTPException(status_code=404,detail="Todo Not Found")
-
-@app.put('linux/{id}')
-async def update_todo(id: int, todo:Todo):
-    try:
-        store_todo[id]=todo
-        return store_todo[id]
-    except:
-        raise HTTPException(status_code=404,detail="Todo Not Found")
-
-@app.get('/todo/{id}')
-async def delete_todo(id:int):
-    try:
-        obj=store_todo[id]
-        store_todo.pop(id)
-        return obj
-    except:
-        raise HTTPException(status_code=404,detail="Todo Not Found")
+@app.post('/language/')
+async def language(name:str=Form(...),type:str=Form(...)):
+    return {"name":name,"type":type}
